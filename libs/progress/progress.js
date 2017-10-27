f296a243a613ceadf9321996a25954c3475df3e6
po4erk <po4erk@mail.ru>
2017-10-27 21:51:10 +0300

    Add files via upload

--- libs/progress/progress.js
+++ libs/progress/progress.js
@@ -1,67 +1,24 @@
 window.onload = function(){
-    moveProgressBar();
-    moveProgressBarFirst();
-    moveProgressBarSecond();
-    moveProgressBarThird();
-    // on browser resize...
+    moveProgressBar('.progress-wrap','progress-percent','.progress-bar');
+    moveProgressBar('.progress-wrap-1','progress-percent','.progress-bar-1');
+    moveProgressBar('.progress-wrap-2','progress-percent','.progress-bar-2');
+    moveProgressBar('.progress-wrap-3','progress-percent','.progress-bar-3');
     $(window).resize(function() {
-        moveProgressBar();
-        moveProgressBarFirst();
-        moveProgressBarSecond();
-        moveProgressBarThird();
+        moveProgressBar('.progress-wrap','progress-percent','.progress-bar');
+        moveProgressBar('.progress-wrap-1','progress-percent','.progress-bar-1');
+        moveProgressBar('.progress-wrap-2','progress-percent','.progress-bar-2');
+        moveProgressBar('.progress-wrap-3','progress-percent','.progress-bar-3');
     });
 
-    // SIGNATURE PROGRESS
-    function moveProgressBar() {
-      console.log("moveProgressBar");
-        var getPercent = ($('.progress-wrap').data('progress-percent') / 100);
-        var getProgressWrapWidth = $('.progress-wrap').width();
+    function moveProgressBar(x,y,z) {
+       
+        var getPercent = ($(x).data(y) / 100);
+        var getProgressWrapWidth = $(x).width();
         var progressTotal = getPercent * getProgressWrapWidth;
-        var animationLength = 1500;
+        var animationLength = 2500;
         
-        // on page load, animate percentage bar to data percentage length
-        // .stop() used to prevent animation queueing
-        $('.progress-bar').stop().animate({
+        $(z).stop().animate({
             left: progressTotal
         }, animationLength);
-    }
-    function moveProgressBarFirst() {
-      console.log("moveProgressBarFirst");
-        var getPercentFirst = ($('.progress-wrap-1').data('progress-percent') / 100);
-        var getProgressWrapWidthFirst = $('.progress-wrap-1').width();
-        var progressTotalFirst = getPercentFirst * getProgressWrapWidthFirst;
-        var animationLengthFirst = 1500;
-        
-        // on page load, animate percentage bar to data percentage length
-        // .stop() used to prevent animation queueing
-        $('.progress-bar-1').stop().animate({
-            left: progressTotalFirst
-        }, animationLengthFirst);
-    }
-    function moveProgressBarSecond() {
-      console.log("moveProgressBarSecond");
-        var getPercentSecond = ($('.progress-wrap-2').data('progress-percent') / 100);
-        var getProgressWrapWidthSecond = $('.progress-wrap-2').width();
-        var progressTotalSecond = getPercentSecond * getProgressWrapWidthSecond;
-        var animationLengthSecond = 1500;
-        
-        // on page load, animate percentage bar to data percentage length
-        // .stop() used to prevent animation queueing
-        $('.progress-bar-2').stop().animate({
-            left: progressTotalSecond
-        }, animationLengthSecond);
-    }
-    function moveProgressBarThird() {
-      console.log("moveProgressBarThird");
-        var getPercentThird = ($('.progress-wrap-3').data('progress-percent') / 100);
-        var getProgressWrapWidthThird = $('.progress-wrap-3').width();
-        var progressTotalThird = getPercentThird * getProgressWrapWidthThird;
-        var animationLengthThird = 1500;
-        
-        // on page load, animate percentage bar to data percentage length
-        // .stop() used to prevent animation queueing
-        $('.progress-bar-3').stop().animate({
-            left: progressTotalThird
-        }, animationLengthThird);
     }
 }

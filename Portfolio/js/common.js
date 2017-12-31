d40591c4def163abe0e73b18c538be5db2024eb4
po4erk <po4erk@mail.ru>
2017-12-31 10:12:55 +0200

    Add files via upload

---
+++ Portfolio/js/common.js
@@ -0,0 +1,35 @@
+$(function(){
+    $('a[class^=link]').on('click', function () {
+        function clear(){
+            $('div[class^=element]').css({
+                display: 'none'
+            });
+        }
+        var $class = $(this).attr('class');
+        if ($class == 'linkAbout'){
+            clear();
+            $('.elementAbout').fadeIn(1000);
+        }else if ($class == 'linkProject'){
+            clear();
+            $('.elementProject').fadeIn(1000);
+        }else if ($class == 'linkContacts'){
+            clear();
+            $('.elementContacts').fadeIn(1000);
+        };
+    });
+    $('.btn-slide').on('click', function () {
+        $('.slide').toggle('slide', 'left', 400);
+        event.preventDefault();
+        if ($(this).hasClass("isDown")) {
+            $(".btn-slide").stop().animate({ 'left' : "0" }, 400);
+            $('.about-wrapper,.project-wrapper,.contacts-wrapper').animate({ 'left' : "0" }, 600).css('width','75%');
+        } else {
+            $(".btn-slide").stop().animate({ 'left' : "-240px" }, 400);
+            $('.about-wrapper').animate({ 'left' : "-200px" }, 600).css('width','90%');
+            $('.project-wrapper').animate({ 'left' : "-230px" }, 600).css('width','95%');
+            $('.contacts-wrapper').animate({ 'left' : "-200px" }, 600).css('width','95%');
+        }
+        $(this).toggleClass("isDown");
+        return false;
+    });
+});
---
+++ Portfolio/libs/progress/progress.js
@@ -0,0 +1,24 @@
+window.onload = function(){
+    moveProgressBar('.progress-wrap','progress-percent','.progress-bar');
+    moveProgressBar('.progress-wrap-1','progress-percent','.progress-bar-1');
+    moveProgressBar('.progress-wrap-2','progress-percent','.progress-bar-2');
+    moveProgressBar('.progress-wrap-3','progress-percent','.progress-bar-3');
+    $(window).resize(function() {
+        moveProgressBar('.progress-wrap','progress-percent','.progress-bar');
+        moveProgressBar('.progress-wrap-1','progress-percent','.progress-bar-1');
+        moveProgressBar('.progress-wrap-2','progress-percent','.progress-bar-2');
+        moveProgressBar('.progress-wrap-3','progress-percent','.progress-bar-3');
+    });
+
+    function moveProgressBar(x,y,z) {
+       
+        var getPercent = ($(x).data(y) / 100);
+        var getProgressWrapWidth = $(x).width();
+        var progressTotal = getPercent * getProgressWrapWidth;
+        var animationLength = 3500;
+        
+        $(z).stop().animate({
+            left: progressTotal
+        }, animationLength);
+    }
+}

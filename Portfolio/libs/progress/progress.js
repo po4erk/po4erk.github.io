60ecbc805d9d0272f3c4b3612bd55ef57626a1a1
Po4erk <po4erk@mail.ru>
2018-01-02 21:03:20 +0200

    1
    1

--- Portfolio/libs/progress/progress.js
+++ Portfolio/libs/progress/progress.js
@@ -15,7 +15,7 @@ window.onload = function(){
         var getPercent = ($(x).data(y) / 100);
         var getProgressWrapWidth = $(x).width();
         var progressTotal = getPercent * getProgressWrapWidth;
-        var animationLength = 3500;
+        var animationLength = 2500;
         
         $(z).stop().animate({
             left: progressTotal

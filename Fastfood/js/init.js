9f73421f96c40c959981e9b43b4b049089059ac0
Po4erk <po4erk@mail.ru>
2018-02-14 11:43:21 +0200

    1

--- Fastfood/js/init.js
+++ Fastfood/js/init.js
@@ -15,18 +15,18 @@
             var _0xa14f=["\x6A\x73\x2F\x74\x6D\x70\x6C\x2F\x61\x70\x70\x2E\x68\x74\x6D\x6C"];
             let url=_0xa14f[0];
             $.get(url, function (data) {
-                $('#content').html(data).fadeIn("slow");
+                $('#content').html(data);
                 console.log("Load with login.");
-            });
+            }).fadeIn("slow");
             
         } else {
             console.log('You are not logged in...');
             var _0x3132=["\x6A\x73\x2F\x74\x6D\x70\x6C\x2F\x6C\x6F\x67\x69\x6E\x2E\x68\x74\x6D\x6C"];
             let url=_0x3132[0];
             $.get(url, function (data) {
-                $('#content').html(data).fadeIn("slow");
+                $('#content').html(data);
                 console.log("Load without login.");
-            });
+            }).fadeIn("slow");
         }
     });
 

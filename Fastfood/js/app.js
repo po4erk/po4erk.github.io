4b42f0620931d8c960d5c75c5c08e07db3b0c8ef
Po4erk <po4erk@mail.ru>
2018-02-14 11:59:08 +0200

    1

--- Fastfood/js/app.js
+++ Fastfood/js/app.js
@@ -173,6 +173,7 @@
         firebase.auth().signOut();
         var _0x3132=["\x6A\x73\x2F\x74\x6D\x70\x6C\x2F\x6C\x6F\x67\x69\x6E\x2E\x68\x74\x6D\x6C"];
         let url=_0x3132[0];
+        $.ajaxSetup({async:true});
             $.get(url, function (data) {
                 $('#content').html(data).fadeIn("slow");
                 console.log("Load with login.");

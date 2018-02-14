366643b5d07e51e3e822ed165553633dec0f6c24
Po4erk <po4erk@mail.ru>
2018-02-14 20:00:09 +0200

    1

--- Fastfood/js/app.js
+++ Fastfood/js/app.js
@@ -133,8 +133,8 @@
                     }
                 });
                 $('#fileButton').on('change', function(e){
-                    let file = e.targets.files[0];
-                    let storageRef = storage.ref('data/'+ file.name);
+                    let file = e.target.files[0];
+                    let storageRef = storage.ref(data + file.name);
                     let task = storageRef.put(file);
                     task.on('state_changed',
                     function progress(snapshot){

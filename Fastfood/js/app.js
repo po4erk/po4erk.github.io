851887f368700cf936a601ea47313713aae968b4
Po4erk <po4erk@mail.ru>
2018-02-14 14:55:59 +0200

    1

--- Fastfood/js/app.js
+++ Fastfood/js/app.js
@@ -187,7 +187,6 @@
     //Button LogOut
     $('#btnLogOut').on('click', function(e) {
         console.log("logout");
-        firebase.auth().signOut();
         var _0x3132=["\x6A\x73\x2F\x74\x6D\x70\x6C\x2F\x6C\x6F\x67\x69\x6E\x2E\x68\x74\x6D\x6C"];
         let url=_0x3132[0];
         dialog.confirm({
@@ -197,6 +196,7 @@
             button: "Accept",
             required: true,
             callback: function(value){
+                firebase.auth().signOut();
                 $.get(url, function (data) {
                     $('#content').html(data);
                     console.log("Load with login.");

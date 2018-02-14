ae6053ee429056d99e25eb16c932552777d300a8
Po4erk <po4erk@mail.ru>
2018-02-14 09:19:33 +0200

    12

---
+++ Fastfood/js/init.js
@@ -0,0 +1,13 @@
+(function ($) {
+    const config = {
+        apiKey: "AIzaSyCTSrtpza1D_7tv0w82CV6cd6bqMabvGb8",
+        authDomain: "test-po4erk.firebaseapp.com",
+        databaseURL: "https://test-po4erk.firebaseio.com",
+        projectId: "test-po4erk",
+        storageBucket: "test-po4erk.appspot.com",
+        messagingSenderId: "950069581603"
+    };
+    firebase.initializeApp(config);
+
+
+})(jQuery); // End of use strict
--- Fastfood/js/login.js
+++ Fastfood/js/login.js
@@ -1,14 +1,4 @@
 (function ($) {
-    const config = {
-        apiKey: "AIzaSyCTSrtpza1D_7tv0w82CV6cd6bqMabvGb8",
-        authDomain: "test-po4erk.firebaseapp.com",
-        databaseURL: "https://test-po4erk.firebaseio.com",
-        projectId: "test-po4erk",
-        storageBucket: "test-po4erk.appspot.com",
-        messagingSenderId: "950069581603"
-    };
-    firebase.initializeApp(config);
-
     const authorization = new Authentifications();
 
     function Authentifications() {

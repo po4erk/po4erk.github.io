2079cb7db0cc656f7515e1bd372391b184f7d2b2
Po4erk <po4erk@mail.ru>
2018-02-15 11:57:45 +0200

    1

--- Fastfood/js/app.js
+++ Fastfood/js/app.js
@@ -81,6 +81,10 @@
                 });
                 $('#result').html(result);
 
+                storage.ref(data).getDownloadURL().then(function(url){
+                    $('.image').attr('src', url);
+                });
+
                 //Changes for title and address
                 $('.title').on('click', function(e){
                     let newTitle = prompt('Enter a new title: ');

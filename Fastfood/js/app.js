514c1cdaf835a5d5c246b05ebd962bf96cf42739
Po4erk <po4erk@mail.ru>
2018-02-16 21:59:05 +0200

    1

--- Fastfood/js/app.js
+++ Fastfood/js/app.js
@@ -203,7 +203,8 @@
     //Button LogOut
     $('#btnLogOut').on('click', function(e) {
         console.log("logout");
-        window.location.hash = $(this).attr('href').substr(0,$(this).attr('href').length-5);
+        window.location.hash = $(this).attr('href');
+        console.log(window.location.hash);
         var _0x3132=["\x6A\x73\x2F\x74\x6D\x70\x6C\x2F\x6C\x6F\x67\x69\x6E\x2E\x68\x74\x6D\x6C"];
         let url=_0x3132[0];
         firebase.auth().signOut();

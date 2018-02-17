ce4dc35eda047041cfc1d6a335bb4c4cdeedcba9
Po4erk <po4erk@mail.ru>
2018-02-17 21:13:23 +0200

    1

--- Fastfood/js/app.js
+++ Fastfood/js/app.js
@@ -238,6 +238,7 @@
     });
 
     $(document).on('click','#addCancel',function(e){
+        $('#content').html('');
         var _0xa14f=["\x6A\x73\x2F\x74\x6D\x70\x6C\x2F\x61\x70\x70\x2E\x68\x74\x6D\x6C"];
             let url=_0xa14f[0];
             $.get(url, function (data) {
